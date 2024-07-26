949c5f1bcba342957f59373267da875e051931a7
Himanshu-1294 <59471536+Himanshu-1294@users.noreply.github.com>
2024-07-26 05:37:35 +0000

    Name - Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket1" {
-	bucket = "neeha-first-bucket"
+	bucket = "himanshug-first-bucket"
 }
 
 resource "aws_s3_bucket" "bucket2" {
@@ -12,10 +12,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "neeha-s3bucket-date"
+    bucket         = "himanshug-s3bucket-20240726"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "neeha-date-dblocks"
+    dynamodb_table = "himanshug-20240726-dblocks"
     encrypt        = true
   }
 }
